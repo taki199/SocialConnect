d00360e7f60e9fc140ae1afa1d2128825286369c
taki199 <christotaki22@gmail.com>
2024-07-17 00:17:01 +0200

    story ring

--- src/app/components/Stories.tsx
+++ src/app/components/Stories.tsx
@@ -7,52 +7,52 @@ const Stories = () => {
         <div className="flex gap-8 w-max">
             {/* Story */}
             <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2 ring-orange-500"/>
                 <span className="font-medium">Ricky</span>
             </div>
              {/* Story */}
              <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2 ring-orange-500"/>
                 <span className="font-medium">Ricky</span>  
             </div>
              {/* Story */}
              <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2 ring-orange-500"/>
                 <span className="font-medium">Ricky</span>
             </div>
              {/* Story */}
              <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2 ring-orange-500 "/>
                 <span className="font-medium">Ricky</span>
             </div>
              {/* Story */}
              <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2 ring-orange-500"/>
                 <span className="font-medium">Ricky</span>
             </div>
              {/* Story */}
              <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2 ring-orange-500"/>
                 <span className="font-medium">Ricky</span>
             </div>
              {/* Story */}
              <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2 ring-orange-500"/>
                 <span className="font-medium">Ricky</span>
             </div>
              {/* Story */}
              <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2 ring-orange-500"/>
                 <span className="font-medium">Ricky</span>
             </div>
              {/* Story */}
              <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2 ring-orange-500"/>
                 <span className="font-medium">Ricky</span>
             </div>
              {/* Story */}
              <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2 ring-gray-500"/>
                 <span className="font-medium">Ricky</span>
             </div>
 
