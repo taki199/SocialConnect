774382bd1c2a3badc4c7a91c4defbf7b7d417b24
taki199 <christotaki22@gmail.com>
2024-08-05 23:40:03 +0200

    updating posts

--- src/app/components/feed/Post.tsx
+++ src/app/components/feed/Post.tsx
@@ -11,19 +11,21 @@ const Post = ({post}:{post:FeedPostType}) => {
         {/* USER */}
         <div className="flex items-center justify-between">
             <div className="flex items-center gap-4">
-                <Image src="https://images.pexels.com/photos/21945949/pexels-photo-21945949/free-photo-of-a-woman-looking-out-the-window.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" height={40} width={40} alt="" className="w-10 h-10 rounded-full"/>
-                <span className="font-medium">Angela Bride</span>
+                <Image src={post.user.avatar || "/noAvatar.png"} height={40} width={40} alt="" className="w-10 h-10 rounded-full"/>
+                <span className="font-medium">
+                {(post.user.name && post.user.surname)? post.user.name +" "+ post.user.surname : post.user.username}
+                </span>
             </div>
             <Image src="/more.png" height={16} width={16} alt="" />
         </div>
         {/* DESCRIPTION */}
         <div className="flex flex-col gap-4">
-            <div className="w-full min-h-96 relative">
-            <Image src="https://images.pexels.com/photos/23638650/pexels-photo-23638650/free-photo-of-default.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+           {post.img &&<div className="w-full min-h-96 relative">
+            <Image src={post.img} 
             fill
             className="object-cover rounded-md" alt="" />
-            </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem soluta nam repellat ea, placeat nihil inventore quam autem? Quidem est vero ea debitis tempora labore reiciendis eum laudantium? Incidunt, dignissimos.</p>
+            </div>}
+            <p>{post.desc}</p>
         </div>
         {/* iNTERACTION */}
         <div className="flex items-center justify-between text-sm my-4">
