e5fd28b06ef14a84e677c7ca1312d34a2ed50d00
taki199 <christotaki22@gmail.com>
2024-08-08 22:28:08 +0200

    post actions

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -262,3 +262,33 @@ export const addComment = async (postId: any, desc: string) => {
     throw new Error("Something went wrong!");
   }
 };
+export const addPost = async (formData: FormData, img: string) => {
+  const desc = formData.get("desc") as string;
+
+  const Desc = z.string().min(1).max(255);
+
+  const validatedDesc = Desc.safeParse(desc);
+
+  if (!validatedDesc.success) {
+    //TODO
+    console.log("description is not valid");
+    return;
+  }
+  const { userId } = auth();
+
+  if (!userId) throw new Error("User is not authenticated!");
+
+  try {
+    await prisma.post.create({
+      data: {
+        desc: validatedDesc.data,
+        userId,
+        img,
+      },
+    });
+
+    revalidatePath("/");
+  } catch (err) {
+    console.log(err);
+  }
+};
