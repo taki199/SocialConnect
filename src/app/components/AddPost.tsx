3fefc32baf8c1d6a3364413f501933093b7e3f91
taki199 <christotaki22@gmail.com>
2024-08-08 22:53:06 +0200

    posts done

--- src/app/components/AddPost.tsx
+++ src/app/components/AddPost.tsx
@@ -1,66 +1,90 @@
-import prisma from "@/lib/client";
-import { auth } from "@clerk/nextjs/server";
-import Image from "next/image"
+"use client";
 
+import { useUser } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { CldUploadWidget } from "next-cloudinary";
+import Image from "next/image";
+import { useState } from "react";
+import AddPostButton from "./AddPostButton";
+import { addPost } from "@/lib/actions";
 
 const AddPost = () => {
+  const { user, isLoaded } = useUser();
+  const [desc, setDesc] = useState("");
+  const [img, setImg] = useState<any>();
 
-  const{userId}=auth()
-  
-  const testAction =async(formData:FormData)=>{
-    "use server"
-
-    if(!userId) return;
-    const desc=formData.get("desc") as string;
-    try {
-     const res= await prisma.post.create({
-        data:{
-          userId:userId,
-          desc:desc,
-        }
-      })
-      console.log(res)
-    } catch (error) {
-      console.log(error)
-    }
-   
+  if (!isLoaded) {
+    return "Loading...";
   }
+
   return (
-    <div className="p-4 bg-white rounded-lg shadow-md flex gap-4 justify-between text-sm">
-        {/* AVATAR */}
-        <Image src="https://images.pexels.com/photos/8259263/pexels-photo-8259263.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" width={48} height={48} alt="" className="w-12 h-12 object-cover rounded-full"/>
-          {/* Post */}
-          <div className="flex-1">
-            {/* Text Input */}
-            <form action="" className="flex gap-4">
-             <textarea placeholder="what's on your mind?" className="bg-slate-100 flex-1 rounded-lg p-2" name="desc"></textarea>
-             <Image src="/emoji.png" width={20} height={20} alt="" className="w-5 h-5 cursor-pointer self-end"/>
-             <button>Send</button>
-             </form>
-            {/* POST OPTIONS */}
-            <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
-                <div className="flex items-center gap-2 cursor-pointer ">
-                <Image src="/addimage.png" width={20} height={20} alt="" />
-                Photo
+    <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
+      {/* AVATAR */}
+      <Image
+        src={user?.imageUrl || "/noAvatar.png"}
+        alt=""
+        width={48}
+        height={48}
+        className="w-12 h-12 object-cover rounded-full"
+      />
+      {/* POST */}
+      <div className="flex-1">
+        {/* TEXT INPUT */}
+        <form action={(formData)=>addPost(formData,img?.secure_url || "")} className="flex gap-4">
+          <textarea
+            placeholder="What's on your mind?"
+            className="flex-1 bg-slate-100 rounded-lg p-2"
+            name="desc"
+            onChange={(e) => setDesc(e.target.value)}
+          ></textarea>
+          <div className="">
+            <Image
+              src="/emoji.png"
+              alt=""
+              width={20}
+              height={20}
+              className="w-5 h-5 cursor-pointer self-end"
+            />
+            <AddPostButton />
+          </div>
+        </form>
+        {/* POST OPTIONS */}
+        <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
+          <CldUploadWidget
+            uploadPreset="connect"
+            onSuccess={(result, { widget }) => {
+              setImg(result.info);
+              widget.close();
+            }}
+          >
+            {({ open }) => {
+              return (
+                <div
+                  className="flex items-center gap-2 cursor-pointer"
+                  onClick={() => open()}
+                >
+                  <Image src="/addimage.png" alt="" width={20} height={20} />
+                  Photo
                 </div>
-                <div className="flex items-center gap-2 cursor-pointer">
-                <Image src="/addvideo.png" width={20} height={20} alt="" />
-                video
-                </div>
-                <div className="flex items-center gap-2 cursor-pointer">
-                <Image src="/poll.png" width={20} height={20} alt="" />
-                Poll
-                </div>
-                <div className="flex items-center gap-2 cursor-pointer">
-                <Image src="/addevent.png" width={20} height={20} alt="" />
-                Event
-                </div>
-                
-            </div>
-         </div>
-
+              );
+            }}
+          </CldUploadWidget>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addVideo.png" alt="" width={20} height={20} />
+            Video
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/poll.png" alt="" width={20} height={20} />
+            Poll
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addevent.png" alt="" width={20} height={20} />
+            Event
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddPost
+export default AddPost;
