f153df7bb65bc5f203f195a0571f5e362786eea7
taki199 <christotaki22@gmail.com>
2024-08-09 17:15:26 +0200

    user stories

--- src/app/components/Stories.tsx
+++ src/app/components/Stories.tsx
@@ -1,64 +1,44 @@
-import Image from "next/image"
+import prisma from "@/lib/client";
+import { auth } from "@clerk/nextjs/server";
+import Image from "next/image";
+import StoryList from "./StoryList";
 
+const Stories = async () => {
+  const { userId: currentUserId } = auth();
 
-const Stories = () => {
+  if (!currentUserId) return null;
+
+  const stories = await prisma.story.findMany({
+    where: {
+      expiresAt: {
+        gt: new Date(),
+      },
+      OR: [
+        {
+          user: {
+            followers: {
+              some: {
+                followerId: currentUserId,
+              },
+            },
+          },
+        },
+        {
+          userId: currentUserId,
+        },
+      ],
+    },
+    include: {
+      user: true,
+    },
+  });
   return (
-    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll  text-xs scrollbar-hide">
-        <div className="flex gap-8 w-max">
-            {/* Story */}
-            <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2 ring-orange-500"/>
-                <span className="font-medium">Ricky</span>
-            </div>
-             {/* Story */}
-             <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2 ring-orange-500"/>
-                <span className="font-medium">Ricky</span>  
-            </div>
-             {/* Story */}
-             <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2 ring-orange-500"/>
-                <span className="font-medium">Ricky</span>
-            </div>
-             {/* Story */}
-             <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2 ring-orange-500 "/>
-                <span className="font-medium">Ricky</span>
-            </div>
-             {/* Story */}
-             <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2 ring-orange-500"/>
-                <span className="font-medium">Ricky</span>
-            </div>
-             {/* Story */}
-             <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2 ring-orange-500"/>
-                <span className="font-medium">Ricky</span>
-            </div>
-             {/* Story */}
-             <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2 ring-orange-500"/>
-                <span className="font-medium">Ricky</span>
-            </div>
-             {/* Story */}
-             <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2 ring-orange-500"/>
-                <span className="font-medium">Ricky</span>
-            </div>
-             {/* Story */}
-             <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2 ring-orange-500"/>
-                <span className="font-medium">Ricky</span>
-            </div>
-             {/* Story */}
-             <div className="flex flex-col items-center gap-2 cursor-pointer">
-                <Image src="https://images.pexels.com/photos/20643866/pexels-photo-20643866/free-photo-of-modern-skyscraper-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2 ring-gray-500"/>
-                <span className="font-medium">Ricky</span>
-            </div>
-
-        </div>
+    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide">
+      <div className="flex gap-8 w-max">
+        <StoryList stories={stories} userId={currentUserId}/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Stories
+export default Stories;
