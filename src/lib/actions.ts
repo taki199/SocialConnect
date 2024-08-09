b80cde982e6e03641ba914b7e7ffb3e2def33985
taki199 <christotaki22@gmail.com>
2024-08-09 16:59:25 +0200

    delete post server action

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -327,3 +327,20 @@ export const addStory = async (img: string) => {
     console.log(err);
   }
 };
+export const deletePost = async (postId: any) => {
+  const { userId } = auth();
+
+  if (!userId) throw new Error("User is not authenticated!");
+
+  try {
+    await prisma.post.delete({
+      where: {
+        id: postId,
+        userId,
+      },
+    });
+    revalidatePath("/")
+  } catch (err) {
+    console.log(err);
+  }
+};
